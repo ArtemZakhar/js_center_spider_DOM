fa172668e3dfba4bde0b552c95acbf5f7dd9e3bc
Artem Zakharchuk <artemzakharchuk@gmail.com>
2024-07-04 15:10:32 +0300

    make build

---
+++ index.461f6742.js
@@ -0,0 +1,2 @@
+document.querySelector(".spider").style.cssText="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%)";
+//# sourceMappingURL=index.461f6742.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,5 @@
+"use strict";
+const spider = document.querySelector(".spider");
+spider.style.cssText = `position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%)`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
